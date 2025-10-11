f16a29fd521a28fa0684e119e042d73ed67b405f
NassirAlawar <nassir.alawar@gmail.com>
2025-10-10 20:53:04 -0700

    Updated with PR feedback

--- superwarp.lua
+++ superwarp.lua
@@ -56,7 +56,6 @@ packets = require('packets')
 require('coroutine')
 config = require('config')
 resources = require('resources')
-require('vectors')
 
 require('sendall')
 require('fuzzyfind')
@@ -377,10 +376,10 @@ local function find_npc(needles)
     for index, npc_data in pairs(needles) do
         local npc = windower.ffxi.get_mob_by_index(index)
         if npc and npc.valid_target then
-            vec[1] = npc.x - me.x
-            vec[2] = npc.y - me.y
-            local npc_distance = vec:length()
-            if not target_npc or (npc_distance < distance) then
+            local dx = npc.x - me.x
+            local dy = npc.y - me.y
+            local npc_distance = math.sqrt((dx * dx) + (dy * dy))
+            if npc_distance < distance then
                 target_npc = npc
                 distance = npc_distance
                 npc_key = npc_data.key
